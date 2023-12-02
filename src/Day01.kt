59fa3291564632bc62fd3bc0bb96293107e62c32
Ivan Nikitin <vantoozz@gmail.com>
2023-12-01 20:11:40 -0500

    + day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,46 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val digits = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9",
+    )
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1(input: List<String>): Int =
+        input.sumOf { line ->
+            "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt()
+        }
+
+    fun part2(input: List<String>): Int =
+        input.map { line ->
+            line.foldIndexed(StringBuilder()) { offset, decoded, char ->
+                decoded.apply {
+                    char.takeIf { it.isDigit() }?.let { append(it) }
+                        ?: run {
+                            digits.forEach { (word, digit) ->
+                                if (line.startsWith(word, offset)) {
+                                    append(digit)
+                                }
+                            }
+                        }
+                }
+            }.toString()
+        }.let {
+            part1(it)
+        }
+
+    check(part1(readInput("Day01_test")) == 142)
+    check(part2(readInput("Day01_test2")) == 281)
+    check(part2(readInput("Day01_test3")) == 68)
 
     val input = readInput("Day01")
+
     part1(input).println()
     part2(input).println()
 }
