17268d78d9665dacf979f96ff095401cd909309c
Ivan Nikitin <vantoozz@gmail.com>
2023-12-09 14:23:38 -0500

    ~ day 9

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -10,28 +10,29 @@ fun main() {
             line.split(" ").map { it.toInt() }
         }
 
-    tailrec fun expand(numbers: List<Int>, result: List<List<Int>> = emptyList()): List<List<Int>> {
-        ((result.takeUnless { it.isEmpty() } ?: listOf(numbers))
-                + listOf(numbers.zipWithNext { left, right -> right - left }))
+    tailrec fun expand(result: List<List<Int>>): List<List<Int>> {
+        (result + listOf(result.last().zipWithNext { left, right -> right - left }))
             .let { expanded: List<List<Int>> ->
-                return if (expanded.last().all { it == 0 }) expanded else expand(expanded.last(), expanded)
+                return if (expanded.last().all { it == 0 }) {
+                    expanded.reversed()
+                } else {
+                    expand(expanded)
+                }
             }
     }
 
     fun part1(originalInput: List<String>) =
         parseInput(originalInput).sumOf {
-            expand(it).reversed()
-                .fold(0) { value: Int, list ->
-                    list.last() + value
-                }
+            expand(listOf(it)).fold(0) { value: Int, list ->
+                list.last() + value
+            }
         }
 
     fun part2(originalInput: List<String>) =
         parseInput(originalInput).sumOf {
-            expand(it).reversed()
-                .fold(0) { value: Int, list ->
-                    list.first() - value
-                }
+            expand(listOf(it)).fold(0) { value: Int, list ->
+                list.first() - value
+            }
         }
 
     check(part1(readInput("day09/test1")) == 114)
